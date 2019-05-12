cc2d334331076e76bfeafab2c76369871ed93f7e
Mark Veltzer <mark.veltzer@gmail.com>
2019-05-13 00:18:56 +0300

    minor fixes

--- micro_services/build.sh
+++ micro_services/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 docker build --tag dapp1:latest --file Dockerfile1 .
 docker build --tag dapp2:latest --file Dockerfile2 .
-# everything is build, take a loot
+# everything is built, take a look
 docker images
--- micro_services/run_dockers.sh
+++ micro_services/run_dockers.sh
@@ -2,4 +2,4 @@
 docker run -d --expose=8000 -p 8000:8000 dapp1
 docker run -d --expose=8001 -p 8001:8001 dapp2
 # everything is running, take a look
-#docker ps
+docker ps
