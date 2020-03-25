47454930b2440c862ea9732b5198d37df1aac24a
Mark Veltzer <mark.veltzer@gmail.com>
2020-03-25 16:18:18 +0200

    added file to be generated as user and not root

--- data_volume/run.sh
+++ data_volume/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-docker run --detach=true --volume /home/mark/tmp:/data data_volume
+docker run --detach=true --user 1000:1000 --volume /home/mark/tmp:/data data_volume
 # everything is running, take a look
 docker ps
