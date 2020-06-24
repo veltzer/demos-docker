9e65bfa4697e74a91e145eb81279563e5289506d
Mark Veltzer <mark.veltzer@gmail.com>
2020-06-24 14:50:44 +0300

    hello_docker -> hello_python

--- hello_python/build.sh
+++ hello_python/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-docker build --tag hello_docker:latest --file Dockerfile .
+docker build --tag hello_python:latest --file Dockerfile .
 # everything is built, take a look
 docker images
--- hello_python/run.sh
+++ hello_python/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-docker run --detach hello_docker
+docker run --detach hello_python
 # everything is running, take a look
 docker ps
