85b812b76f68e369688b1a9f68b5f9a25acdfbc3
Mark Veltzer <mark.veltzer@gmail.com>
2020-06-25 13:58:42 +0300

    better solution

--- data_volume/run.sh
+++ data_volume/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-docker run --detach=true --user 1000:1000 --volume /home/mark/tmp:/data data_volume
+docker run --detach --user 1000:1000 --volume /home/mark/tmp:/data data_volume
 # everything is running, take a look
 docker ps
