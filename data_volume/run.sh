7efdc8db17357450baa9486b88544c8d2997f7fc
Mark Veltzer <mark.veltzer@gmail.com>
2020-06-25 14:26:18 +0300

    not hardcoding user id and group id anymore

--- data_volume/run.sh
+++ data_volume/run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-docker run --detach --user 1000:1000 --volume /home/mark/tmp:/data data_volume
+UID=`getent passwd mark | cut -d ":" -f 3`
+GID=`getent passwd mark | cut -d ":" -f 4`
+docker run --detach --user $UID:$GID --volume /home/mark/tmp:/data data_volume
 # everything is running, take a look
 docker ps
