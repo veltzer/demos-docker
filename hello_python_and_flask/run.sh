82d7a53d2ac15efa5427ba5c906b8b367383cd0a
Mark Veltzer <mark.veltzer@gmail.com>
2020-11-05 16:37:11 +0200

    web server with python and flask

--- hello_python_and_flask/run.sh
+++ hello_python_and_flask/run.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-docker run -it hello_python_and_flask
+# docker run --network=host -it hello_python_and_flask
+docker run -p 8080:8080 -it hello_python_and_flask
+# this DOES NOT work
+# docker run -p 8080 -it hello_python_and_flask
