920a3710342a50d326d33f6063355126e4d2e3a9
Mark Veltzer <mark.veltzer@gmail.com>
2020-11-08 12:04:14 +0200

    watchdog example

--- python_rootless_with_watchdog/app.py
+++ python_rootless_with_watchdog/app.py
@@ -3,8 +3,11 @@
 import time
 import sys
 import os
+import random
 
 i = 0
+crash_lines=random.randint(10,20)
+
 while True:
     print(f"hello from python inside docker! ({i}) I am {os.getuid()}...")
     time.sleep(1)
@@ -13,7 +16,13 @@ while True:
         for line in f:
             lines+=1
     print(f"the file has {lines} lines")
+
+    if lines % crash_lines == 0:
+        # kazam! we crash!
+        raise ValueError("help me! I'm dying!")
+
     with open("data.txt", "a") as f:
         f.write("hello\n")
+    print(f"added a line to the file...")
     sys.stdout.flush()
     i=i+1
---
+++ python_rootless_with_watchdog/watchdog.py
@@ -0,0 +1,8 @@
+#!/usr/local/bin/python3
+
+import os
+
+while True:
+    # run the app application
+    print(f"running it again...")
+    os.system("/app.py")
