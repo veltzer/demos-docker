1da10fefb4ad6aadb756939c3e03c9d7b18c2b64
Mark Veltzer <mark.veltzer@gmail.com>
2020-12-07 13:38:00 +0200

    improved example

--- scratch_and_bash_dynamic/before_build.sh
+++ scratch_and_bash_dynamic/before_build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 cp /bin/bash .
-cp `ldd /bin/bash | tr -s " " | cut -f 3 -d " " | grep -v "^$"` .
+\rm -rf libs
+mkdir libs
+cp `ldd /bin/bash | tr -s " " | cut -f 3 -d " " | grep -v "^$"` libs
 cp /lib64/ld-linux-x86-64.so.2 .
