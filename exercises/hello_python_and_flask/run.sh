4a9f87951c5445880eba996d6ad987de08ee0362
Mark Veltzer <mark.veltzer@gmail.com>
2021-02-11 15:55:16 +0200

    new exercise

--- exercises/hello_python_and_flask/run.sh
+++ exercises/hello_python_and_flask/run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # docker run --network=host -it hello_python_and_flask
-docker run -p 8080:8080 -it hello_python_and_flask
+# docker run -p 8080:8080 -it hello_python_and_flask
+docker run -p 8080:8080 -d hello_python_and_flask
 # this DOES NOT work
 # docker run -p 8080 -it hello_python_and_flask
