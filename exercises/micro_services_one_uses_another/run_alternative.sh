ca1fae4830b396ff0ccedbe8cef891e677d659fb
Mark Veltzer <mark.veltzer@gmail.com>
2021-02-14 15:16:32 +0200

    another solution

--- exercises/micro_services_one_uses_another/run_alternative.sh
+++ exercises/micro_services_one_uses_another/run_alternative.sh
@@ -4,5 +4,7 @@
 
 # In this version of the run script we do not use "--network host"
 # which is a security hazard. We will try another configuration which is safer...
-id=$(docker run -d app1)
-docker run -d -p 8080:8080 --link $id app2
+docker run -d --name app1 app1
+APP1_IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' app1)
+echo $APP1_IP
+docker run -d --name app2 -p 8080:8080 --link app1:app1 app2 ./app2.py $APP1_IP
