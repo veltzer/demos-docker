4c5b74f345ad784e5bfb684ae54424507c4df090
Mark Veltzer <mark.veltzer@gmail.com>
2021-12-10 12:57:58 +0200

    more stuff

---
+++ examples/security_running_as_user/app2.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# first lets see who we are...
+id
+whoami
+echo $UID $EUID
+
+if [[ -f "/etc/passwd" ]]
+then
+	echo "yes, there is a '/etc/passwd' file inside the container"
+else
+	echo "no, there isn't a '/etc/passwd' file inside the container"
+fi
+
+# now lets see that we can write data to a file...
+while [[ true ]]
+do
+	echo "I'm still running and my id is $UID..."
+	echo "some more data" >> datafile.txt
+	sleep 1
+done
