08fe61a3e567bc0becc0298a58ad8208e1c15737
Mark Veltzer <mark.veltzer@gmail.com>
2021-12-12 21:32:04 +0200

    commit message

--- exercises/volume_data/writer.py
+++ exercises/volume_data/writer.py
@@ -1,20 +1,9 @@
 #!/usr/bin/env python3
 
 import time
-import os.path
-import sys
 
-folder=sys.argv[1]
-filename=os.path.join(folder, "data.txt")
-
-if os.path.isfile(filename):
-    with open(filename, "rt") as f:
-        for line in f:
-            pass
-        i = int(line)+1
-else:
-    i=0
-with open(filename, "at") as f:
+i=0
+with open("/data/data.txt", "w") as f:
     while True:
         f.write(f"{i}\n")
         f.flush()
