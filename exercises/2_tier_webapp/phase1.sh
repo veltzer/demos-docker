c17cbe8bc700a2bd84efc2df4e02bad21de0c7dc
Mark Veltzer <mark.veltzer@gmail.com>
2021-12-19 18:02:35 +0200

    phase1 of 2 tier

---
+++ exercises/2_tier_webapp/phase1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -eux
+
+export MYSQL_ROOT_PASSWORD=pass
+if docker inspect mysql > /dev/null
+then
+	docker kill mysql > /dev/null
+	docker rm mysql > /dev/null
+fi
+docker run --name mysql --detach --network host -e MYSQL_ROOT_PASSWORD mysql:latest > /dev/null
+sleep 10
+mysql --defaults-file=mysql.conf --protocol tcp <<FINISH
+CREATE DATABASE foo;
+USE foo;
+CREATE TABLE bar (value int);
+INSERT INTO bar (value) VALUES(2);
+INSERT INTO bar (value) VALUES(3);
+INSERT INTO bar (value) VALUES(5);
+INSERT INTO bar (value) VALUES(8);
+SELECT * FROM bar;
+FINISH
--- exercises/volume_shared_data/app.py
+++ exercises/volume_shared_data/app.py
@@ -9,4 +9,5 @@ interval=int(sys.argv[2])
 while True:
     with open("data/data.txt", "a") as f:
         f.write(f"{name_of_app} was here!\n")
+        print(f"{name_of_app} wrote another line")
     time.sleep(interval)
