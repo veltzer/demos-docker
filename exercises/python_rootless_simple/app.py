78a96b12a6731e7c21664a73981d13d02517932a
Mark Veltzer <mark.veltzer@gmail.com>
2022-03-27 14:03:23 +0300

    commit message

--- exercises/python_rootless_simple/app.py
+++ exercises/python_rootless_simple/app.py
@@ -1,11 +1,11 @@
-#!/usr/local/bin/python3
+#!/usr/bin/env python3
 
 import time
 import sys
 
 i = 0
 while True:
-    print("hello from python inside docker! ({})".format(i))
-    time.sleep(5)
+    print(f"hello from python inside docker! ({i})")
+    time.sleep(1)
     sys.stdout.flush()
     i=i+1
