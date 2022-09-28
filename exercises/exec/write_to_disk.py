066cfd2dd9622bc64acfd89ab7bb7341dd89d7a9
Mark Veltzer <mark.veltzer@gmail.com>
2022-09-28 16:00:35 +0300

    no commit message given

--- exercises/exec/write_to_disk.py
+++ exercises/exec/write_to_disk.py
@@ -5,8 +5,7 @@ import time
 with open("data.txt", "w") as f:
     i=0
     while True:
-        print(f"writing line {i}...")
-        f.write(f"this is line number {i}\n") 
+        f.write(f"this is line number {i}...\n")
         f.flush()
         time.sleep(1)
         i+=1
