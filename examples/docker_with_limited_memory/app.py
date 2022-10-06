1a3846152982b189bc04936f214c20e77b95a0ec
Mark Veltzer <mark.veltzer@gmail.com>
2022-10-06 11:24:25 +0300

    no commit message given

--- examples/docker_with_limited_memory/app.py
+++ examples/docker_with_limited_memory/app.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
 
 import time
-import sys
 import random
 
-i = 0
 big_array = []
 while True:
-    add_array = [random.randint(0, 100) for _ in range(1000000)]
+    add_array = [random.randint(0, 100) for _ in range(300000)]
     big_array.extend(add_array)
-    print("hello from python inside docker! ({})".format(i))
-    sys.stdout.flush()
-    time.sleep(5)
-    i=i+1
+    print(f"size of array is [{len(big_array)}]...")
+    time.sleep(1)
