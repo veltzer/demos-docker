6b3fdd6505c0bac1205c13b51801730df4e70a4d
Mark Veltzer <mark.veltzer@gmail.com>
2022-11-22 14:07:53 +0200

    no commit message given

--- exercises/post_mortem/write_to_disk.py
+++ exercises/post_mortem/write_to_disk.py
@@ -1,11 +1,13 @@
 #!/usr/local/bin/python3
 
 import time
+import sys
 
 with open("data.txt", "w") as f:
     i=0
     while True:
         print(f"writing line {i}...")
+        sys.stdout.flush()
         f.write(f"this is line number {i}\n") 
         f.flush()
         time.sleep(1)
