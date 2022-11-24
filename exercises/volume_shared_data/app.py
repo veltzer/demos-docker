9d4b5659f913c214e6ed745d5ad299d33181db8f
Mark Veltzer <mark.veltzer@gmail.com>
2022-11-24 11:44:32 +0200

    linted the application

--- exercises/volume_shared_data/app.py
+++ exercises/volume_shared_data/app.py
@@ -1,23 +1,37 @@
 #!/usr/bin/env python3
 
+"""
+This is the solution to the volumes exercise
+"""
+
 import time
 import sys
 import os.path
 import os
 
-if len(sys.argv) == 3:
-    name_of_app=sys.argv[1]
-    interval=int(sys.argv[2])
-else:
-    name_of_app="default_name"
-    interval=1
+def main():
+    """ main entry point """
+    if len(sys.argv) == 3:
+        name_of_app=sys.argv[1]
+        interval=int(sys.argv[2])
+    else:
+        name_of_app="default_name"
+        interval=1
 
-if not os.path.isdir("data"):
-    print("data folder was not found, creating...")
-    os.mkdir("data")
+    if not os.path.isdir("data"):
+        print("data folder was not found, creating...")
+        os.mkdir("data")
 
-while True:
-    with open("data/data.txt", "a") as f:
-        f.write(f"[{name_of_app}] was here!\n")
-        print(f"[{name_of_app}] wrote another line")
-    time.sleep(interval)
+    counter=0
+    while True:
+        with open("data/data.txt", "a", encoding="utf-8") as stream:
+            stream.write(f"[{name_of_app}] at time [{counter}]...\n")
+            stream.flush()
+            print(f"[{name_of_app}] at time [{counter}]...")
+            sys.stdout.flush()
+        time.sleep(interval)
+        counter+=interval
+
+
+if __name__=="__main__":
+    main()
