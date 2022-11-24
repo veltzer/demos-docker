dabfdb1941683d886ee949919ea22ef0f48be39f
Mark Veltzer <mark.veltzer@gmail.com>
2022-11-24 11:30:03 +0200

    no commit message given

--- exercises/volume_shared_data/app.py
+++ exercises/volume_shared_data/app.py
@@ -2,12 +2,22 @@
 
 import time
 import sys
+import os.path
+import os
 
-name_of_app=sys.argv[1]
-interval=int(sys.argv[2])
+if len(sys.argv) == 3:
+    name_of_app=sys.argv[1]
+    interval=int(sys.argv[2])
+else:
+    name_of_app="default_name"
+    interval=1
+
+if not os.path.isdir("data"):
+    print("data folder was not found, creating...")
+    os.mkdir("data")
 
 while True:
     with open("data/data.txt", "a") as f:
-        f.write(f"{name_of_app} was here!\n")
-        print(f"{name_of_app} wrote another line")
+        f.write(f"[{name_of_app}] was here!\n")
+        print(f"[{name_of_app}] wrote another line")
     time.sleep(interval)
