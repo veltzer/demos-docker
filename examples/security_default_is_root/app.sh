ae3a4753d7ae425f7a320c9552b9ba9fa1f8aaae
Mark Veltzer <mark.veltzer@gmail.com>
2022-12-29 16:35:08 +0200

    no commit message given

--- examples/security_default_is_root/app.sh
+++ examples/security_default_is_root/app.sh
@@ -2,3 +2,10 @@
 id
 whoami
 echo "UID is $UID and EUID is $EUID"
+/sbin/capsh --print
+ps -ef
+# this does not work anyway...
+# which reboot
+# echo "are you ready for a reboot????..."
+# sleep 5
+# reboot
