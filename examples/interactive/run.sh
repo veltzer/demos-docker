2f201a376441ddebbf4d8b46a53b6ce3be660192
Mark Veltzer <mark.veltzer@gmail.com>
2023-03-26 11:22:48 +0300

    no commit message given

--- examples/interactive/run.sh
+++ examples/interactive/run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 image_name=$(basename $PWD)
 # this will succeed
-# docker run --interactive "${image_name}"
+docker run --interactive "${image_name}"
 # this will fail
 # docker run "${image_name}"
 # this will also fail
 # docker run --detach "${image_name}"
-docker run --interactive --tty --detach "${image_name}"
+# docker run --interactive --tty --detach "${image_name}"
