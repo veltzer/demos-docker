feb3ace4818721061242d78b4176dca4ee51cd29
Mark Veltzer <mark.veltzer@gmail.com>
2023-04-26 12:52:24 +0300

    more linting

--- exercises/python_rootless_with_user_ownership/app.py
+++ exercises/python_rootless_with_user_ownership/app.py
@@ -1,21 +1,32 @@
 #!/usr/bin/env python3
 
+"""
+Logging application which knows how to continue from where it left off
+"""
+
 import time
 import sys
 import os
 import os.path
 
-i = 0
-while True:
-    print(f"hello from python inside docker! ({i}) I am {os.getuid()}...")
-    time.sleep(1)
-    lines=0
-    if os.path.isfile('data.txt'):
-        with open("data.txt", "r") as f:
-            for line in f:
-                lines+=1
-    print(f"the file has {lines} lines")
-    with open("data.txt", "a") as f:
-        f.write("hello\n")
-    sys.stdout.flush()
-    i=i+1
+
+def main():
+    """ main entry point """
+    i = 0
+    while True:
+        print(f"hello from python inside docker! ({i}) I am {os.getuid()}...")
+        time.sleep(1)
+        lines=0
+        if os.path.isfile("data.txt"):
+            with open("data.txt", "r", encoding="utf-8") as stream:
+                for _line in stream:
+                    lines+=1
+        print(f"the file has {lines} lines")
+        with open("data.txt", "a", encoding="utf-8") as stream:
+            stream.write("hello\n")
+        sys.stdout.flush()
+        i=i+1
+
+
+if __name__ == "__main__":
+    main()
