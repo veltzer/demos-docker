c9884cfd0482277ab61ab6986119f6b3ad8d90bc
Mark Veltzer <mark.veltzer@gmail.com>
2023-12-30 01:52:10 +0200

    new pydmt version

--- config/python.py
+++ config/python.py
@@ -1,7 +1,10 @@
-config_requires = []
-dev_requires = []
-install_requires = []
-make_requires = [
+from typing import List
+
+
+config_requires: List[str] = []
+dev_requires: List[str] = []
+install_requires: List[str] = []
+make_requires: List[str] = [
     "flask",
     "mysql-connector-python",
     "pyvardump",
@@ -22,5 +25,5 @@ make_requires = [
     "mypy",
     "pydmt",
 ]
-test_requires = []
+test_requires: List[str] = []
 requires = config_requires + install_requires + make_requires + test_requires
