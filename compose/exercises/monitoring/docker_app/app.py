757a79230ab271eb64c352876500fffdf7b1e066
Mark Veltzer <mark.veltzer@gmail.com>
2024-03-23 12:48:57 +0200

    no commit message given

---
+++ compose/exercises/monitoring/docker_app/app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+Web server
+"""
+
+
+import flask
+
+
+app = flask.Flask("app")
+
+
+@app.route("/")
+def root():
+    """ root url """
+    return "<head><body>Hello World</body></head>"
+
+app.run(port=8080, host="0.0.0.0")
