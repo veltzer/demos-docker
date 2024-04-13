2c1148bf04b3940af88f7fe9366b6403f26bbb52
Mark Veltzer <mark.veltzer@gmail.com>
2024-04-13 03:53:49 +0300

    no commit message given

---
+++ scripts/login.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+# This script logs in to your container registry
+
+# site="docker.io"
+site="ghcr.io"
+
+# get user and password
+username=$(jq -r ".[\"${site}\"][\"user\"]" ~/.passwords.json)
+password=$(jq -r ".[\"${site}\"][\"password\"]" ~/.passwords.json)
+# debug
+echo "username is ${username}"
+echo "password is ${password}"
+echo "site is ${site}"
+
+# login
+echo "${password}" | docker login --username "${username}" --password-stdin "${site}"
--- scripts/push.sh
+++ scripts/push.sh
@@ -2,6 +2,7 @@
 image=$(basename "${PWD}")
 version="latest"
 username="veltzer"
-site="docker.io"
+# site="docker.io"
+site="ghcr.io"
 docker tag "${image}:latest" "${site}/${username}/${image}:${version}"
 docker push "${site}/${username}/${image}:${version}"
