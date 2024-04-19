0501edb8cbd31bda5619b2636fd0fc87f99cca42
Mark Veltzer <mark.veltzer@gmail.com>
2024-04-20 01:06:37 +0300

    no commit message given

---
+++ compose/exercises/nodejs_postgresql/app/server.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.get('/', (req, res) => {
+  res.send('Hello World from express javascript server inside docker container!')
+})
+
+app.listen(port, () => {
+  console.log(`Contact me at http://localhost:${port}`)
+})
