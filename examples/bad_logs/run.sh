71d10315016b0a2048f1c1ed3d44ee87e2e414ea
Mark Veltzer <mark.veltzer@gmail.com>
2024-04-24 07:33:20 +0300

    no commit message given

--- examples/bad_logs/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-image_name=$(basename "${PWD}")
-# --log-driver json-file is the default
-docker run --detach "${image_name}"
--- examples/container_arguments/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-image_name=$(basename "${PWD}")
-docker run -it -e A=6 "${image_name}"
