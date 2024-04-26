50fea25f1c1580bf413f0a8592f27849d3f80cd2
Mark Veltzer <mark.veltzer@gmail.com>
2024-04-26 23:05:29 +0300

    no commit message given

--- compose/exercises/nodejs_postgresql/.auto.enter.sh
+++ compose/exercises/nodejs_postgresql/.auto.enter.sh
@@ -1,5 +1,5 @@
 site="postgres"
 env_db_user="postgres"
-env_db_password=$(pass show "${site}/${env_db_user}")
+env_db_password=$(pass show "passwords/${site}/${env_db_user}")
 export env_db_user
 export env_db_password
