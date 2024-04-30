4e7e2818a5df5e3f26ab0a01a76e3b5a01bdbd56
Mark Veltzer <mark.veltzer@gmail.com>
2024-04-30 16:11:43 +0300

    no commit message given

---
+++ compose/exercises/health_check/first/app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+"""
+Super simple web server
+"""
+
+from flask import Flask
+
+
+app = Flask("app")
+
+
+@app.route("/")
+def root():
+    """ catch all urls """
+    print("in root")
+    return "<html><body><h1>python with flask in a docker<h1></body><html>"
+
+
+app.run(port=8080, host="0.0.0.0")
