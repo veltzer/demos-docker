fac8709de06929a945b3debb54d0fde814645351
Mark Veltzer <mark.veltzer@gmail.com>
2024-06-05 08:58:47 +0300

    no commit message given

--- scripts/login.sh
+++ scripts/login.sh
@@ -2,12 +2,12 @@
 
 # This script logs in to your container registry
 
-# site="docker.io"
-site="ghcr.io"
+site="docker.io"
+# site="ghcr.io"
 username="veltzer"
 
 # get password
-password=$(pass show "${site}/${username}")
+password=$(pass show "passwords/${site}/${username}")
 # debug
 # echo "username is ${username}"
 # echo "password is ${password}"
