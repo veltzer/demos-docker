88815ebbe76167c9da0dfb8b4d7289a53898b34e
Mark Veltzer <mark.veltzer@gmail.com>
2024-06-15 11:10:36 +0300

    no commit message given

--- exercises/java_build_gradle_in_docker/run.sh
+++ exercises/java_build_gradle_in_docker/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -e
-java -classpath build/classes/java/main com.company.HelloWorld
+./gradlew run
+# java -classpath build/classes/java/main com.company.HelloWorld
 # java -jar build/libs/java_basic-1.1.jar hello.HelloWorld
