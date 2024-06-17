18aaac676c465aec1666a37115250ae3d48f077c
Mark Veltzer <mark.veltzer@gmail.com>
2024-06-17 12:47:10 +0300

    no commit message given

--- exercises/java_build_maven_in_docker/src/main/java/com/company/HelloWorld.java
+++ exercises/java_build_maven_in_docker/src/main/java/com/company/HelloWorld.java
@@ -4,7 +4,7 @@ import org.apache.commons.lang3.StringUtils;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        String text = "This is a simple Maven app using Apache Commons Lang....";
+        String text = "This is a simple Maven app using Apache Commons Lang.";
         String reversed = StringUtils.reverse(text);
 
         System.out.println("Original: " + text);
