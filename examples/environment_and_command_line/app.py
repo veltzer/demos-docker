d5a6971371c9a813ae0139b432102614340fac7e
Mark Veltzer <mark.veltzer@gmail.com>
2024-12-22 10:16:40 +0200

    no commit message given

---
+++ examples/environment_and_command_line/app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+Print all environment variables and command line arguments
+"""
+
+import os
+import sys
+
+print(sys.argv)
+print(os.environ)
--- examples/set_environment_variables/app.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-"""
-Print an environment variable called "MARK"
-"""
-
-import os
-
-print(f"MARK is set to {os.environ['MARK']}")
